936fcb4f774f0e4ad02b65e3166b3e4cfa9dc201
Saksham Khare <sakshamkhare19@gmail.com>
2024-05-05 21:52:48 +0530

    useEffect working

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,21 +1,27 @@
 import { useState, useEffect } from "react";
-import { createLogger } from "vite";
 
 export default function Counter () {
     const [count, setCount] = useState(0);
+    const [name, setName] = useState("");
 
     useEffect(function myEffect() {
         console.log("Effect called");
-    })
+    }, [count])
 
     const increment = () => {
         setCount(c => c + 1);
+    }
+
+    const handleChange = (e) => {
+        setName(e.target.value);
     }
 
     return (
         <div>
             <h1>{count}</h1>
             <button onClick={increment}>+1</button>
+            <p>Name: {name}</p>
+            <input type="text" value={name} onChange={handleChange} />
         </div>
     )
 }
